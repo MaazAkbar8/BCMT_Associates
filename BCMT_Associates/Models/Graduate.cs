bb26464c5da582acedf2d006abbf92b6626be2ec
Dell <https://github.com/MaazAkbar8/Die-Roll-Dise.git>
2024-11-24 08:25:49 -0800

    Add project files.

---
+++ BCMT_Associates/Models/Graduate.cs
@@ -0,0 +1,21 @@
+﻿namespace BCMT_Associates.Models
+{
+    public class Graduate
+    {
+
+
+
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string Course { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string CertificationNumber { get; set; }
+        public string CNIC { get; set; }
+        public string Address { get; set; }
+        public string EmergencyContact { get; set; }
+    }
+}
